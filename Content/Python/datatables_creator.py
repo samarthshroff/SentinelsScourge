ca861a4e34b24adfe146d5a65355f2e1d1298bbf
Samarth Shroff <samarth.shroff@gmail.com>
2025-03-28 01:49:30 -0400

    Python WIP (Paused for now) - Need to find out the rowstruct the imported csv adheres to and use that to make the data table before import so that it can then import any and all kinds of legit csvs.

--- Content/Python/datatables_creator.py
+++ Content/Python/datatables_creator.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import tkinter
 from pathlib import Path
@@ -61,7 +62,11 @@ class DataTablesCreator:
         file_names = os.listdir(self.csv_path)
         unreal.log(file_names)
 
+        #struct_scripts:[] = ["/Script/VampireSurvivorClone.EnemyMetaData", "/Script/VampireSurvivorClone.VSPlayerAttributeSet"]
+
+
         row_struct = unreal.find_object(None, "/Script/VampireSurvivorClone.EnemyMetaData")
+        unreal.log(f"the row_struct is:: {row_struct}")
         if not row_struct:
             unreal.log_error("Row structure data FWeaponMetaData not found")
             raise Exception("Row structure data not found")
@@ -69,11 +74,68 @@ class DataTablesCreator:
         self.data_table_factory.set_editor_property("struct", row_struct)
 
         for file_name in file_names:
+            #self.validate_against_datatable(f"{self.csv_path}/{file_name}", file_name, "/Script/VampireSurvivorClone.EnemyMetaData", row_struct)
             new_datatable = self.asset_tools.create_asset(f"DT_{Path(file_name).stem}", self.directory, unreal.DataTable, self.data_table_factory)
 
             if new_datatable is not None:
                 new_datatable.set_editor_property("ignore_missing_fields", True)
                 new_datatable.fill_from_csv_file(f"{self.csv_path}/{file_name}")
+
+    def validate_against_datatable(self, file_path, file_name, row_struct_name, row_struct):
+        # temp_dt = self.asset_tools.create_asset(f"DT_{Path(file_name).stem}", self.directory, unreal.DataTable,
+        #                                               self.data_table_factory)
+
+        temp_dt = unreal.DataTable(None, "TempDT")
+        temp_dt.set_editor_property("row_struct", row_struct)
+
+        unreal.log(f"Column Names: {temp_dt.get_column_names()}")
+        datatable_column_names = temp_dt.get_column_names()
+        # row_struct = unreal.EditorAssetLibrary.load_asset(row_struct_name)
+        #
+        # unreal.log(f"the file_path: {file_path}")
+        # if not isinstance(row_struct, unreal.ScriptStruct):
+        #     unreal.log(f"Invalid row struct: {row_struct}")
+        #     return False
+        #
+        # unreal.log(f"row struct dir: {dir(row_struct)}")
+        # attributes = [attr for attr in dir(row_struct) if not attr.startswith('__') and not callable(getattr(row_struct, attr))]
+        # for name in attributes:
+        #     print(f"Variable Name: {name} | Value: {getattr(row_struct, name)}")
+        #
+        # attributes = row_struct.__dict__
+        # for name, value in attributes.items():
+        #     print(f"Variable Name: {name} | Value: {value}")
+        #unreal.log(f"row_struct type: {row_struct.__dict__.keys()}")
+        # Get the property names of the RowStruct
+        #struct_properties = [prop.get_name() for prop in row_struct.get_properties()]
+
+       # #Load the CSV file and extract headers
+       #  with open(file_path, mode='r') as file:
+       #      csv_file = csv.reader(file)
+       #      csv_headers = next(csv_file)
+       #      unreal.log(f"csv_headers: {csv_headers}")
+       #      for header in csv_headers[1:]:
+       #          if header not in datatable_column_names:
+       #              unreal.log(f"CSV header {header} is not present in the datatable {temp_dt.get_name()}")
+       #              break
+
+            # try:
+            #     value = row_struct.__getattribute__(csv_headers[1])
+            #     unreal.log("Value found in the row struct")
+            # except AttributeError:
+            #     unreal.log("Value not found in the row struct")
+            #     # Skip if it's not an editor property
+            #     pass
+
+
+            #columnProp =
+
+
+        # Validate headers
+        #if csv_headers != struct_properties:
+
+
+
 
 
 
