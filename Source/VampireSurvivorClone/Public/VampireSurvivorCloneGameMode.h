8b25bd711023efb8c6ecab959ae51191ebbd5281
Samarth Shroff <samarth.shroff@gmail.com>
2025-05-14 01:02:40 -0400

    Added few more channels for better collision detection between land, player, enemies, pick-ups and projectiles.

--- Source/VampireSurvivorClone/Public/VampireSurvivorCloneGameMode.h
+++ Source/VampireSurvivorClone/Public/VampireSurvivorCloneGameMode.h
@@ -8,6 +8,9 @@
 
 #define ECC_LandChannel ECC_GameTraceChannel1
 #define ECC_ProjectileChannel ECollisionChannel::ECC_GameTraceChannel2
+#define ECC_EnemyChannel ECollisionChannel::ECC_GameTraceChannel3
+#define ECC_PickupItemsChannel ECollisionChannel::ECC_GameTraceChannel4
+#define ECC_PlayerCharacterChannel ECollisionChannel::ECC_GameTraceChannel5
 
 /**
  * 
